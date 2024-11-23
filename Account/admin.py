b6df0b1ded9309174e2bb6a5238214fb3af933bf
Sydul_amin <sydulamin6@gmail.com>
2024-11-23 18:59:57 +0600

    new model design and admin panel data customized

--- Account/admin.py
+++ Account/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
-from . models import test_table
+from .models import test_table, Main_test
+
 # Register your models here.
 
-admin.site.register(test_table)
+admin.site.register(Main_test)
+
+
+class TestTableAdmin(admin.ModelAdmin):
+    list_display = ('name', 'gender', 'get_father_name', 'get_mother_name', 'get_nationality')
+    list_filter = ('parents__Nationality',)
+
+    def get_father_name(self, obj):
+        return obj.parents.Father_name if obj.parents else "No Parent"
+
+    get_father_name.short_description = "Father Name"
+
+    def get_mother_name(self, obj):
+        return obj.parents.Mother_name if obj.parents else "No Parent"
+
+    get_mother_name.short_description = "Mother Name"
+
+    def get_nationality(self, obj):
+        return obj.parents.Nationality if obj.parents else "No Nationality"
+
+    get_nationality.short_description = "Nationality"
+
+
+admin.site.register(test_table, TestTableAdmin)
--- Account/models.py
+++ Account/models.py
@@ -3,9 +3,28 @@ from django.db import models
 
 # Create your models here.
 
-
-class test_table(models.Model):
-    name = models.CharField(max_length=10)
+class Main_test(models.Model):
+    Father_name = models.CharField(max_length=10)
+    Mother_name = models.CharField(max_length=10)
+    Nationality = models.CharField(max_length=10)
 
     def __str__(self):
-        return self.name
+        return self.Father_name
+
+
+class test_table(models.Model):
+    CHOICE = [
+        ('Male', 'Male',),
+        ('Female', 'Female'),
+        ('Others', 'Others')
+    ]
+
+    name = models.CharField(max_length=10)
+    parents = models.ForeignKey(Main_test, on_delete=models.CASCADE, null=True, blank=True)
+    gender = models.CharField(choices=CHOICE, default='Female', max_length=6)
+
+    def __str__(self):
+        if not self.parents:
+            return self.name
+        else:
+            return f"{self.parents.Father_name}'s Sons and nationality is {self.parents.Nationality} "
