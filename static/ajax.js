c86d44dcfdbc180948f0b393785206e271eaf1ba
Sydul_amin <sydulamin6@gmail.com>
2024-12-10 14:26:42 +0600

    ajex update dome to es6

--- static/ajax.js
+++ static/ajax.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const counterValue = document.getElementById("counter-value");
     const incrementBtn = document.getElementById("increment-btn");
     const decrementBtn = document.getElementById("decrement-btn");
-
+    
     // Fetch the initial counter value
     fetch("/get-counter/")
         .then(response => response.json())
@@ -10,21 +10,22 @@ document.addEventListener("DOMContentLoaded", function () {
             counterValue.textContent = data.value;
         });
 
-    // Increment counter
-    incrementBtn.addEventListener("click", () => {
-        fetch("/increment-counter/")
-            .then(response => response.json())
-            .then(data => {
-                counterValue.textContent = data.value;
-            });
+    // Decrement counter
+    decrementBtn.addEventListener("click", async () => {
+        const res = await fetch("/decrement-counter/")
+        const data = await res.json()  
+        counterValue.textContent = data.value  
     });
 
-    // Decrement counter
-    decrementBtn.addEventListener("click", () => {
-        fetch("/decrement-counter/")
-            .then(response => response.json())
-            .then(data => {
-                counterValue.textContent = data.value;
-            });
+    // increment counter
+    incrementBtn.addEventListener("click", async () => {      
+        const res = await fetch("/increment-counter/")
+        const data =  await res.json()
+        counterValue.textContent = data.value
     });
+
 });
+
+
+
+
