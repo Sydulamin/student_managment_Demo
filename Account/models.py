5b4fadeb83ccad2dcf8c139071b63e812db12624
Sydul_amin <sydulamin6@gmail.com>
2024-12-18 15:34:47 +0600

    email verification done

--- Account/models.py
+++ Account/models.py
@@ -4,7 +4,9 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=15, blank=True, null=True)
-    image = models.ImageField(upload_to='profile_image/', default='def.png', null=True, blank=True)
+    image = models.ImageField(upload_to='profile_image/', default='def.png',max_length=255, null=True, blank=True)
+    otp = models.PositiveIntegerField(null=True, blank=True)
+    is_verified = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
@@ -35,7 +37,6 @@ class test_table(models.Model):
             return self.name
         else:
             return f"{self.parents.Father_name}'s Sons and nationality is {self.parents.Nationality} "
-
 
 
 class Counter(models.Model):
--- Account/views.py
+++ Account/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render, redirect
 from .models import CustomUser, Counter
 from django.contrib import messages
 from django.contrib.auth import authenticate, login as auth_login
+from django.core.mail import send_mail
+import random
+from django.conf import settings
+
 
 def home(request):
     return render(request, 'home/home.html')
@@ -10,21 +14,23 @@ def home(request):
 
 def login(request):
     if request.method == "POST":
-        username = request.POST.get('uname') 
+        username = request.POST.get('uname')
         password = request.POST.get('pass1')
 
         if username and password:
             user = authenticate(username=username, password=password)
             if user:
-                auth_login(request, user)
-                return redirect('home')
+                if user.is_verified:
+                    auth_login(request, user)
+                    return redirect('home')
+                else:
+                    messages.error(request, "Please verify your account.")
+                    return redirect('otp_verify')
             else:
                 messages.error(request, "Invalid username or password.")
                 return redirect('login')
 
     return render(request, 'auth/login.html')
-
-
 
 
 def reg(request):
@@ -44,38 +50,65 @@ def reg(request):
             messages.error(request, "Passwords do not match.")
         else:
             try:
-                user = CustomUser (
+                otp = random.randint(0000, 9999)
+                user = CustomUser(
                     first_name=first_name,
                     last_name=last_name,
                     username=username,
                     email=email,
                     phone_number=number,
-                    image=image
+                    image=image,
+                    otp=otp
                 )
                 user.set_password(password)
                 user.save()
-                messages.success(request, "Account created successfully.")
-                return redirect('login')
+
+                send_mail(
+                    "Your Account verification OTP",
+                    f"Here is Your otp : {otp}.",
+                    settings.EMAIL_HOST_USER,
+                    [email],
+                )
+
+                messages.success(request, "Check your email inbox for the verification otp.")
+                return redirect('otp_verify')
             except Exception as e:
                 messages.error(request, str(e))
                 return redirect('reg')
 
     return render(request, 'auth/registration.html')
-      
+
+
+def otp_verify(request):
+    if request.method == 'GET':
+        otp = request.GET.get('otp')
+        if otp:
+            user = CustomUser.objects.get(otp=otp)
+            if user:
+                user.is_verified = True
+                user.save()
+                return redirect('login')
+            else:
+                messages.error(request, "Wrong verification otp.")
+                return redirect('otp_verify')
+    return render(request, 'auth/otp.html')
+
 
 def page(request):
     return render(request, 'counter.html')
 
 
 def get_counter(request):
-    counter = Counter.objects.get(id=1)  
+    counter = Counter.objects.get(id=1)
     return JsonResponse({'value': counter.value})
+
 
 def increment_counter(request):
     counter = Counter.objects.get(id=1)
     counter.value += 1
     counter.save()
     return JsonResponse({'value': counter.value})
+
 
 def decrement_counter(request):
     counter = Counter.objects.get(id=1)
